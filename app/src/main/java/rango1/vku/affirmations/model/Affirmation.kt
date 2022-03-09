ba7172f7f51778a2445ae94c53d1e4e48dd53f10
Rango2 <97595370+phanvanduc02@users.noreply.github.com>
2022-03-09 11:04:53 +0700

    Initial commit

---
+++ app/src/main/java/rango1/vku/affirmations/model/Affirmation.kt
@@ -0,0 +1,11 @@
+package rango1.vku.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    ){
+
+}
